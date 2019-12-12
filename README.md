7c146ab3fbf4efba27359269b6eba177e4537354
Hayden McAfee <hayden@outlook.com>
2019-12-11 21:52:48 -0800

    Hey I made a little screensaver looking thing

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 1. Open VS Command Prompt x64
 2. `set SFMLPATH=C:\Path\To\SFML\Root\Dir`
 3. `code .`
-4. Build
+4. Create `bin` and `obj` dirs
+5. Build
+6. Copy DLLs from SFMLPATH\bin to the bin folder
+7. Run
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <SFML/Graphics.hpp>
 
+static const unsigned int FRAME_RATE = 60u;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // Set up our rendering window
+    sf::RenderWindow window(
+        sf::VideoMode(1920, 1080),
+        "Hello World",
+        sf::Style::Titlebar | sf::Style::Close
+    );
+    window.setFramerateLimit(FRAME_RATE);
+
+    // Init font + text
+    sf::Font segoeFont;
+    segoeFont.loadFromFile("C:\\Windows\\Fonts\\seguibl.ttf");
+    sf::Text helloWorldText;
+    helloWorldText.setFont(segoeFont);
+    helloWorldText.setCharacterSize(64);
+    helloWorldText.setFillColor(sf::Color::White);
+    helloWorldText.setString("HELLO WORLD!");
+
+    // Physics (if you can call it that)
+    int velocityX = 2;
+    int velocityY = 2;
+    float posX = 0.0f;
+    float posY = 0.0f;
+    auto bounds = helloWorldText.getLocalBounds();
+    auto windowSize = window.getSize();
 
     while (window.isOpen())
     {
@@ -14,9 +37,34 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        // Update positions
+        posX += velocityX;
+        posY += velocityY;
+        if (posX < 0)
+        {
+            posX = 0;
+            velocityX *= -1;
+        }
+        else if (posY < 0)
+        {
+            posY = 0;
+            velocityY *= -1;
+        }
+        else if (posX > windowSize.x - bounds.width)
+        {
+            velocityX *= -1;
+            posX = windowSize.x - bounds.width;
+        }
+        else if (posY > windowSize.y - bounds.height)
+        {
+            velocityY *= -1;
+            posY = windowSize.y - bounds.height;
+        }
+        helloWorldText.setPosition(posX, posY);
 
+        // Draw
         window.clear();
-        window.draw(shape);
+        window.draw(helloWorldText);
         window.display();
     }
 
